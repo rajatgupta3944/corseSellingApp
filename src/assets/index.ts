d545824d645bc4e1b6408d262ccac2fed8654317
Rajat Gupta <rajatgupta3944@gmail.com>
2026-02-03 23:09:19 +0530

    Empty cart image added if no course is added in the cart

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import LookfinityLogoIcon from "./Icons/lookfinity-logo.svg";
 import rajatImage from "./Images/home-rajat.jpg"
 import coursesPhoto from "./Images/coursesPhoto.jpg";
+import emptyCart from "./Images/empty-cart.png";
 
-export {LookfinityLogoIcon, rajatImage, coursesPhoto};
+export {LookfinityLogoIcon, rajatImage, coursesPhoto, emptyCart};
--- src/features/cart/pages/Cart.tsx
+++ src/features/cart/pages/Cart.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { useAppSelector } from '../../../app/hooks'
 import CourseCard from '../../../components/common/CourseCard';
 import { Flex } from 'antd';
+import { emptyCart } from '../../../assets';
 
 
 const Cart = () => {
   const { items, totalAmount } = useAppSelector(state => state.cart)
   console.log(items, totalAmount)
   return (
-    <Flex gap={20}>{items?.map((item: any) => {
+    <div>
+    {!items.length ? 
+    <Flex justify='space-around'>
+    <img src={emptyCart} alt="Empty Cart" />
+    </Flex> :
+      <Flex gap={20}>{items?.map((item: any) => {
       return <CourseCard title={item.title} price={item.price} quantity={item.quantity}></CourseCard>
-    })}</Flex>
+    })}</Flex>}
+    </div>
   )
 }
 
