1bcc29d0e05d08c27011c00ecff6dd2cc341e876
Rajat Gupta <rajatgupta3944@gmail.com>
2026-02-07 13:49:50 +0530

    environment configuration

--- src/features/auth/cognito/cognitoAuthConfig.ts
+++ src/features/auth/cognito/cognitoAuthConfig.ts
@@ -1,8 +1,16 @@
+// export const cognitoAuthConfig = {
+//   authority:
+//     "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_b3tmswMrq",
+//   client_id: "38h4mk3scdp6ve3kpjckg8mba7",
+//   redirect_uri: "http://localhost:5173",
+//   response_type: "code",
+//   scope: "phone openid email",
+// };
+
 export const cognitoAuthConfig = {
-  authority:
-    "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_b3tmswMrq",
-  client_id: "38h4mk3scdp6ve3kpjckg8mba7",
-  redirect_uri: "http://localhost:5173",
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   response_type: "code",
-  scope: "phone openid email",
+  scope: "openid email phone",
 };
--- src/features/auth/pages/LogoutButton.tsx
+++ src/features/auth/pages/LogoutButton.tsx
@@ -5,17 +5,16 @@ const LogoutButton = () => {
   const auth = useAuth();
 
   const logout = () => {
-    const clientId = "38h4mk3scdp6ve3kpjckg8mba7";
-    const logoutUri = "http://localhost:5173";
-    const cognitoDomain =
-      "https://ap-south-1b3tmswmrq.auth.ap-south-1.amazoncognito.com";
+    const clientId = import.meta.env.VITE_COGNITO_CLIENT_ID;
+    const logoutUri = import.meta.env.VITE_LOGOUT_URI;
+    const cognitoDomain = import.meta.env.VITE_COGNITO_AUTHORITY;
 
     // clear local tokens
     auth.removeUser();
 
     // redirect to Cognito logout
     window.location.href =
-      `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`
+  `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
   return <CourseButton onClick={logout} label="Logout" type="primary" danger />;
